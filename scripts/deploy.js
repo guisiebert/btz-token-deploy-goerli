00c39650ae59e8b17631067ab237286592d1f430
Gui Siebert <guilhermebsiebert@gmail.com>
2023-08-30 15:17:15 -0300

    bug fixes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,7 +3,6 @@ const { ethers } = require("hardhat");
 async function main() {
     const Bitizens = await ethers.deployContract("Bitizens");
     await Bitizens.waitForDeployment();
-    const address = await Bitizens.getAddress()
 
     console.log(
         `Deployed to ${Bitizens.target}`
